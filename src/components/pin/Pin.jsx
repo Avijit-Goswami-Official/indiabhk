7f2b99da19a60bcb4998298a1ef6254f4a547ba3
Avijit <avijitgoswamiofficial.com>
2024-09-17 16:11:24 +0530

    pin component update

--- src/components/pin/Pin.jsx
+++ src/components/pin/Pin.jsx
@@ -1,6 +1,18 @@
 import { Marker, Popup } from 'react-leaflet'
 import './pin.scss'
 import { Link } from 'react-router-dom'
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// Set up the default icon for markers
+const DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 function Pin({item}){
   return (
